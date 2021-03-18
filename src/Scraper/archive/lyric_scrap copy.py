d3f2fe595d78423e36da02813f915e981add2911
Adrian Romo <edgar.adrian97@gmail.com>
2021-03-18 11:00:23 -0600

    Feat: Added Scraper and cleaning files

---
+++ src/Scraper/archive/lyric_scrap copy.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+
+import pandas as pd
+import re
+m_columns = ['ALink','SName', 'SLink', 'Lyric', 'Idiom']
+
+df = pd.read_csv('lyrics-data.csv', encoding='latin-1', usecols=m_columns)
+df = df[df['Idiom'] == 'ENGLISH']
+df = df.replace({'\[(.*?)\]': ''}, regex=True)
+df = df.replace({'Chorus': ''}, regex=True)
+df = df.replace({'Ûª': 'i'}, regex=True)
+df = df.replace('-',' ')
+df = df.replace('  ',' ')
+df = df.replace('. .',' ')
+df = df.replace('(',' ')
+df = df.replace(')',' ')
+df = df.replace('\n',' ')
+
+df = df.drop_duplicates()
+df = df.reset_index(drop=True)
+print(df['Idiom'].value_counts())
+df.to_csv('output.csv')
+
