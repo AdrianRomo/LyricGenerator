49ee1fbe801993aff1c3f71b2a1ff5d815c0ea5b
Adrian Romo <edgar.adrian97@gmail.com>
2021-05-30 20:49:36 -0500

    Fix: markovify fixed

--- Documentacion/TT/imagenes/Disenio/Pruebas/markovif.py
+++ Documentacion/TT/imagenes/Disenio/Pruebas/markovif.py
@@ -1,38 +1,45 @@
 import markovify
 import random 
+import pandas as pd
+import os
 
 def versos(cantidad):
-	for sentence in range(cantidad):
-	    generatedlyrics = markovifyModel.make_short_sentence(random.choice(lenghtlyric))
-	    print(generatedlyrics)
-	else:
-		print()
+    for sentence in range(cantidad):
+        generatedlyrics = markovifyModel.make_short_sentence(random.choice(lenghtlyric))
+        print(generatedlyrics)
 
 def generarcoros(cantidad):
-	for sentence in range(cantidad):
-	    generatedlyrics = markovifyModel.make_short_sentence(random.choice(lenghtcoros))
-	    coro.append(generatedlyrics)
+    for sentence in range(cantidad):
+        generatedlyrics = markovifyModel.make_short_sentence(random.choice(lenghtcoros))
+        coro.append(generatedlyrics)
 
 def printcoro():
-	for sentence in coro:
-		print(sentence)
-	else:
-		print()
+    for sentence in coro:
+        print(sentence)
 
-generatedlyrics = ()
 lenghtversos = [4, 5, 6]
 lenghtlyric = [80, 100, 120]
 lenghtcoros = [60, 80, 100]
 coro = []
-file = open('lyrics150k.txt', 'r') 
-text = file.read()
-markovifyModel = markovify.Text(text)
+m_columns = ['Lyric']
+for _ in range(4): os.chdir('..')
+path = os.path.abspath(os.getcwd()+"/Datasets" + "/Pop.csv")
+df = pd.read_csv(path, encoding='latin-1', usecols=m_columns)
+markovifyModel = markovify.Text(df['Lyric'])
+
 markovifyModel = markovifyModel.compile()
 generarcoros(random.choice(lenghtversos))
-for beginning in range(2): 
-	versos(random.choice(lenghtversos))
+for beginning in range(1,2):
+    print("\nVerse " + str(beginning))
+    versos(random.choice(lenghtversos))
+print("\nChorus")
 printcoro()
-for medium in range(2): 
-	versos(random.choice(lenghtversos))
+for medium in range(3,4): 
+    print("\nVerse " + str(medium))
+    versos(random.choice(lenghtversos))
+
+print("\nChorus")
 printcoro()
+
+print("\nChorus")
 printcoro()
