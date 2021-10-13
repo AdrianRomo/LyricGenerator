e39ff6625e17b86e687536404928c8197fdb05dd
Adrian Romo <edgar.adrian97@gmail.com>
2021-10-13 02:04:19 -0500

    Update main.py

--- Proyectos-guia/Lyric_Generator/main.py
+++ Proyectos-guia/Lyric_Generator/main.py
@@ -72,6 +72,5 @@ def moderate_action():
         found=found,
     )
 
+app.run(debug=FLASK_ENV == 'development', host='0.0.0.0')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
