13ce3770bc4345c5382b84f1fd24d1d281cd4d16
Adrian Romo <edgar.adrian97@gmail.com>
2021-11-21 16:42:20 -0600

    Feat: Fixed contact form

--- src/Front-end/tt_api_v2/src/components/Form.js
+++ src/Front-end/tt_api_v2/src/components/Form.js
@@ -4,36 +4,55 @@ import { useForm, ValidationError } from '@formspree/react';
 function Form() {
     const [state, handleSubmit] = useForm("xknklaew");
     if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
+        return <p>Thanks for your submission!</p>
+        ;
     }
     return (
         <>
-            <form onSubmit={handleSubmit}>
-            <label htmlFor="email">
-                Email Address
-            </label>
-            <input
-                id="email"
-                type="email" 
-                name="email"
-            />
-            <ValidationError 
-                prefix="Email" 
-                field="email"
-                errors={state.errors}
-            />
-            <textarea
-                id="message"
-                name="message"
-            />
-            <ValidationError 
-                prefix="Message" 
-                field="message"
-                errors={state.errors}
-            />
-            <button type="submit" disabled={state.submitting}>
-                Submit
-            </button>
+        
+            <form onSubmit={handleSubmit} className="p-5 bg-white" name="contactform">
+            <div className="row form-group">
+                <div className="col-md-12 mb-3 mb-md-0">
+                    <label className="font-weight-bold" htmlFor="fullname">Full Name</label>
+                    <input type="text" id="fullname" name="fullname" className="form-control" placeholder="John" />
+                </div>
+            </div>
+            <div className="row form-group">
+                    <div className="col-md-12">
+                        <label className="font-weight-bold" htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" className="form-control" placeholder="your_name@mail.com" />
+                        <ValidationError 
+                            prefix="Email" 
+                            field="email"
+                            errors={state.errors}
+                        />
+                    </div>
+            </div>
+            <div className="row form-group">
+                    <div className="col-md-12">
+                        <label className="font-weight-bold" htmlFor="email">Subject</label>
+                        <input type="text" id="subject" name="subject" className="form-control" placeholder="Enter Subject" />
+                    </div>
+            </div>
+            <div className="row form-group">
+                    <div className="col-md-12">
+                        <label className="font-weight-bold" htmlFor="message">Message</label>
+                        <textarea name="message" id="message" cols={30} rows={5} className="form-control" placeholder="Say hello to us" defaultValue={""} />
+                        <ValidationError 
+                            prefix="Message" 
+                            field="message"
+                            errors={state.errors}
+                        />
+                    </div>
+                </div>
+            <center>
+                    <div className="row form-group">
+                        <div className="col-md-12">
+                            <input type="submit" defaultValue="Send" className="btn btn-primary  py-2 px-4 rounded-0"/>
+                            <input type="reset" className="btn btn-primary  py-2 px-4 rounded-0"/>
+                        </div>
+                    </div>
+            </center>
             </form>
         </>
     )
